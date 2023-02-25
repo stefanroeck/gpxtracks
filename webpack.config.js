521f1d5d519d5a2b4ba46233366cb41e0c142ba8
Stefan Roeck <stefan.roeck@gmail.com>
2023-02-25 11:19:21 +0100

    fix webpack config and file location

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/gpx', to: 'gpx' },
+        { from: 'public/favicon.ico' },
+      ]
+    })
+  ]
+};
