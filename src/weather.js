3257158ac5a63df987e150e1cc9a87f90be1fbdb
Stefan Roeck <stefan.roeck@gmail.com>
2023-03-05 15:00:03 +0100

    handle weather api errors

--- src/weather.js
+++ src/weather.js
@@ -18,18 +18,22 @@ export const getWeather = (lat, lng, day) => {
         console.debug('Fetching weather data', lat, lng, day);
         return fetch(`https://archive-api.open-meteo.com/v1/era5?latitude=${lat}&longitude=${lng}&start_date=${day}&end_date=${day}&daily=weathercode,temperature_2m_max&timezone=CET`)
             .then(async response => {
-                if (response.ok) {
-                    const weatherData = await response.json();
-                    const result = {
-                        lat,
-                        lng,
-                        day,
-                        temperature: weatherData.daily.temperature_2m_max[0] + weatherData.daily_units.temperature_2m_max,
-                        weatherCode: weatherData.daily.weathercode[0],
-                    };
-                    weatherDataCache.push(result);
-                    return result;
+                if (!response.ok) {
+                    return Promise.resolve({
+                        temperature: 'n/a',
+                        weatherCode: 0,
+                    });
                 }
+                const weatherData = await response.json();
+                const result = {
+                    lat,
+                    lng,
+                    day,
+                    temperature: weatherData.daily.temperature_2m_max[0] + weatherData.daily_units.temperature_2m_max,
+                    weatherCode: weatherData.daily.weathercode[0],
+                };
+                weatherDataCache.push(result);
+                return result;
             });
     }
 }
