c4571530bad51a8cbc31e50d329f9d6d7f425d21
Stefan Roeck <stefan.roeck@gmail.com>
2023-11-04 19:38:43 +0100

    display avg speed in info box

--- src/routeInfo.js
+++ src/routeInfo.js
@@ -1,57 +1,52 @@
-import { getWeather, weatherCodeToSymbol } from './weather';
+import { getWeather, weatherCodeToSymbol } from "./weather";
 
 export const renderRouteInfo = (map) => {
+  L.Control.InfoBox = L.Control.extend({
+    infoBoxContent: undefined,
+    infoBox: undefined,
 
-    L.Control.InfoBox = L.Control.extend({
-        infoBoxContent: undefined,
-        infoBox: undefined,
+    onAdd: function () {
+      this.infoBox = L.DomUtil.create("div");
+      L.DomUtil.addClass(this.infoBox, "route-info leaflet-popup-content-wrapper");
+      this.infoBoxContent = L.DomUtil.create("div");
+      L.DomUtil.addClass(this.infoBoxContent, "leaflet-popup-content");
+      this.infoBox.appendChild(this.infoBoxContent);
+      this.hideRouteInfo();
+      return this.infoBox;
+    },
 
-        onAdd: function () {
-            this.infoBox = L.DomUtil.create('div');
-            L.DomUtil.addClass(this.infoBox, 'route-info leaflet-popup-content-wrapper');
-            this.infoBoxContent = L.DomUtil.create('div');
-            L.DomUtil.addClass(this.infoBoxContent, 'leaflet-popup-content');
-            this.infoBox.appendChild(this.infoBoxContent);
-            this.hideRouteInfo();
-            return this.infoBox;
-        },
+    onRemove: function () {
+      // Nothing to do here
+    },
 
-        onRemove: function () {
-            // Nothing to do here
-        },
+    showRouteInfo: async function (mapTrack) {
+      const latlng = mapTrack.getBounds().getCenter();
+      const day = mapTrack._info.duration.start.toISOString().substring(0, 10);
+      const weather = await getWeather(latlng.lat, latlng.lng, day);
+      const text = this._popupText(mapTrack, weather);
+      this.infoBoxContent.innerHTML = text;
+      this.infoBox.style.display = "block";
+    },
 
-        showRouteInfo: async function (mapTrack) {
-            const latlng = mapTrack.getBounds().getCenter();
-            const day = mapTrack._info.duration.start.toISOString().substring(0, 10);
-            const weather = await getWeather(latlng.lat, latlng.lng, day);
-            const text = this._popupText(mapTrack, weather);
-            this.infoBoxContent.innerHTML = text;
-            this.infoBox.style.display = 'block';
-        },
+    hideRouteInfo: function () {
+      this.infoBox.style.display = "none";
+    },
 
-        hideRouteInfo: function () {
-            this.infoBox.style.display = 'none';
-        },
-
-        _formatDate: function (millis) {
-            return new Date(millis).toISOString().slice(11, 19);
-        },
-
-        _popupText: function (track, weather) {
-            return `
+    _popupText: function (track, weather) {
+      return `
               <h5>${track.get_name()}</h4>
               <div class="row"><span class="icon">📅</span>${track.get_start_time().toLocaleDateString()}</div>
               <div class="row"><span class="icon">🏔</span>${Math.round(track.get_distance() / 1000)} km, ${track.get_elevation_gain()} hm</div>
-              <div class="row"><span class="icon">🕑</span>${this._formatDate(track.get_total_time())}</div>
+              <div class="row"><span class="icon">🏃</span>${Math.round(track.get_total_speed() * 10) / 10} km/h</div>
+              <div class="row"><span class="icon">🕑</span>${track.get_duration_string(track.get_total_time(), true)}</div>
               <div class="row"><span class="icon">${weatherCodeToSymbol(weather.weatherCode)}</span>${weather.temperature}</div>
               `;
-        },
+    },
+  });
 
-    });
+  L.control.infoBox = function (opts) {
+    return new L.Control.InfoBox(opts);
+  };
 
-    L.control.infoBox = function (opts) {
-        return new L.Control.InfoBox(opts);
-    }
-
-    return L.control.infoBox({ position: 'topright' }).addTo(map);
-}
+  return L.control.infoBox({ position: "topright" }).addTo(map);
+};
