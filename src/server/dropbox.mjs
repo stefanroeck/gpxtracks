0a753a4e5ff2d1cb02d3ee4a581474a8998223ae
Stefan Roeck <stefan.roeck@gmail.com>
2024-03-08 21:29:17 +0100

    get accessToken dynamically

--- src/server/dropbox.mjs
+++ src/server/dropbox.mjs
@@ -1,21 +1,42 @@
 import fetch from "node-fetch";
 import fs from "fs";
 import { finished } from "stream/promises";
-import { Readable } from "stream";
 
 const SEARCH_TAG = "#longdistancewalk";
 
-const defaultApiHeaders = () => {
+const defaultApiHeaders = (accessToken) => {
   return {
-    Authorization: `Bearer ${process.env.DROPBOX_ACCESS_TOKEN}`,
+    Authorization: `Bearer ${accessToken}`,
     "Content-Type": "application/json",
   };
 };
 
+const getAccessToken = async () => {
+  const formData = new FormData();
+  formData.append("grant_type", "refresh_token");
+  formData.append("client_id", process.env.DROPBOX_APP_KEY);
+  formData.append("client_secret", process.env.DROPBOX_APP_SECRET);
+  formData.append("refresh_token", process.env.DROPBOX_REFRESH_TOKEN);
+
+  const resp = await fetch(`https://api.dropbox.com/oauth2/token`, {
+    method: "POST",
+    body: formData,
+  });
+
+  if (resp.status !== 200) {
+    const text = await resp.text();
+    throw new Error("Failed to get access token: " + resp.status + " - " + text);
+  }
+  const json = await resp.json();
+  return json["access_token"];
+};
+
 export const getTracks = async () => {
+  const accessToken = await getAccessToken();
+
   const response = await fetch("https://api.dropboxapi.com/2/files/search_v2", {
     method: "POST",
-    headers: defaultApiHeaders(),
+    headers: defaultApiHeaders(accessToken),
     body: JSON.stringify({
       query: SEARCH_TAG,
       options: {
